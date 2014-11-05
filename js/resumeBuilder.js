eddd3d7d27e3ff0de7d2f261aa7ea3315d9de6df
Luis Antonio <larc63@gmail.com>
2014-11-05 04:08:54 -0600

    added some prettyness

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,89 +1,75 @@
+function ResumeBuilder() {
+    this.addAll = function () {
+        this.displayBio();
+        this.displayContacts();
+        this.displaySkills();
+        this.displayWorkExperience();
+        this.displayProjects();
+        this.displayEducation();
+        $("#mapDiv").append(googleMap);
+    };
 
-function ResumeBuilder(){
-  this.addAll = function(){
-    this.displayBio();
-    this.displayContacts();
-    this.displaySkills();
-    this.displayWorkExperience();
-    this.displayProjects();
-    this.displayEducation();
-    $("#mapDiv").append(googleMap);
-  };
+    this.displaySkills = function () {
+        $("#skillsDiv").append(HTMLskillsStart);
+        for (var i in data.bio.skills) {
+            $("#skillsDiv").append(HTMLskills.replace("%data%", data.bio.skills[i]));
+        }
+    };
 
-  this.displaySkills = function(){
-    $("#skillsDiv").append(HTMLskillsStart);
-    for(var i in data.bio.skills){
-      $("#skillsDiv").append(HTMLskills.replace("%data%", data.bio.skills[i]));
-    }
-  };
+    this.displayBio = function () {
+        $("#header").prepend(HTMLheaderRole.replace("%data%", data.bio.role));
+        $("#header").prepend(HTMLheaderName.replace("%data%", data.bio.name));
+    };
 
-  this.displayBio = function(){
-    $("#header").prepend(HTMLheaderRole.replace("%data%", data.bio.role));
-    $("#header").prepend(HTMLheaderName.replace("%data%", data.bio.name));
-  };
+    this.displayWorkExperience = function () {
+        for (var job in data.work.jobs) {
+            $("#workExperience").append(HTMLworkStart);
+            $(".work-entry:last").append(HTMLworkEmployer.replace("%data%", data.work.jobs[job].employer).replace("%url%", data.work.jobs[job].url) + HTMLworkTitle.replace("%data%", data.work.jobs[job].title));
+            $(".work-entry:last").append(HTMLworkDates.replace("%data%", data.work.jobs[job].dates));
+            $(".work-entry:last").append(HTMLworkLocation.replace("%data%", data.work.jobs[job].location));
+            $(".work-entry:last").append(HTMLworkDescription.replace("%data%", data.work.jobs[job].description));
+        }
+    };
 
-  this.displayWorkExperience = function(){
-    for(var job in data.work.jobs){
-      $("#workExperience").append(HTMLworkStart);
-      $(".work-entry:last").append(HTMLworkEmployer.replace("%data%", data.work.jobs[job].employer).replace("%url%", data.work.jobs[job].url) + HTMLworkTitle.replace("%data%", data.work.jobs[job].title));
-      $(".work-entry:last").append(HTMLworkDates.replace("%data%", data.work.jobs[job].dates));
-      $(".work-entry:last").append(HTMLworkLocation.replace("%data%", data.work.jobs[job].location));
-      $(".work-entry:last").append(HTMLworkDescription.replace("%data%", data.work.jobs[job].description));
-    }
-  };
+    this.displayProjects = function () {
+        for (var p in data.projects.projects) {
+            $("#projects").append(HTMLprojectStart);
+            var s = HTMLprojectTitle.replace("%data%", data.projects.projects[p].title);
+            s += HTMLprojectDates.replace("%data%", data.projects.projects[p].dates);
+            s += HTMLprojectDescription.replace("%data%", data.projects.projects[p].description);
+            $(".project-entry:last").append(s);
+            if (typeof data.projects.projects[p].images[0] === "string") {
+                $(".project-entry:last").append(HTMLprojectImage.replace("%data%", data.projects.projects[p].images[0]));
+            }
+        }
+    };
 
-  this.displayProjects = function(){
-    for(var p in data.projects.projects){
-      $("#projects").append(HTMLprojectStart);
-      var s = HTMLprojectTitle.replace("%data%", data.projects.projects[p].title);
-      s += HTMLprojectDates.replace("%data%", data.projects.projects[p].dates);
-      s += HTMLprojectDescription.replace("%data%", data.projects.projects[p].description);
-      $(".project-entry:last").append(s);
-      if(typeof data.projects.projects[p].images[0] === "string"){
-        $(".project-entry:last").append(HTMLprojectImage.replace("%data%", data.projects.projects[p].images[0]));
-      }
-      // for(var i in projects.projects[p].images){
-      //   $(".project-entry:last").append(HTMLprojectImage.replace("%data%", projects.projects[p].images[i]));
-      // }
-    }
-  };
+    this.displayContacts = function () {
+        for (c in data.bio.contacts) {
+            $("#footerContacts").append(HTMLcontactGeneric.replace("%data%", data.bio.contacts[c]).replace("%contact%", c));
+            $("#footerContactsInner").append(HTMLcontactGeneric.replace("%data%", data.bio.contacts[c]).replace("%contact%", c));
+        }
+    };
 
-
-  this.displayContacts = function(){
-    for(c in data.bio.contacts){
-//        console.log(c + ": " + data.bio.contacts[c]);
-        $("#footerContacts").append(HTMLcontactGeneric.replace("%data%", data.bio.contacts[c]).replace("%contact%", c));
-        $("#footerContactsInner").append(HTMLcontactGeneric.replace("%data%", data.bio.contacts[c]).replace("%contact%", c));
-    }
-  };
-
-  this.displayEducation = function(){
-      for(var s in data.education.schools){
-        $("#education").append(HTMLschoolStart);
-        $(".education-entry:last").append(HTMLschoolName.replace("%data%", data.education.schools[s].name));
-        $(".education-entry:last").append(HTMLschoolDegree.replace("%data%", data.education.schools[s].degree));
-        $(".education-entry:last").append(HTMLschoolDates.replace("%data%", data.education.schools[s].dates));
-        $(".education-entry:last").append(HTMLschoolLocation.replace("%data%", data.education.schools[s].location));
-        $(".education-entry:last").append(HTMLschoolMajor.replace("%data%", data.education.schools[s].majors[0]));
-      }
-    $("#education").append(HTMLonlineClasses);
-      data.education.onlineCourses.reverse();
-      for (var s in data.education.onlineCourses){
-          var courseString = HTMLonlineStart;
-          courseString += HTMLonlineTitle.replace("%data%", data.education.onlineCourses[s].title);
-          courseString += HTMLonlineSchool.replace("%data%", data.education.onlineCourses[s].school);
-          
-//          var certificateURL = data.education.onlineCourses[s].certificate;
-//          if(typeof certificateURL !== "undefined"){
-//            courseString = courseString + " - <a href='" + data.education.onlineCourses[s].certificate + "'>Certificate</a>";
-//          }
-          
-//          courseString += HTMLonlineDates.replace("%data%", data.education.onlineCourses[s].dates);
-          courseString += HTMLonlineURL.replace("%data%", data.education.onlineCourses[s].url).replace("%data%", data.education.onlineCourses[s].url);
-
-          $("#education").after(courseString);
-      }
-  };
+    this.displayEducation = function () {
+        for (var s in data.education.schools) {
+            $("#education").append(HTMLschoolStart);
+            $(".education-entry:last").append(HTMLschoolName.replace("%data%", data.education.schools[s].name));
+            $(".education-entry:last").append(HTMLschoolDegree.replace("%data%", data.education.schools[s].degree));
+            $(".education-entry:last").append(HTMLschoolDates.replace("%data%", data.education.schools[s].dates));
+            $(".education-entry:last").append(HTMLschoolLocation.replace("%data%", data.education.schools[s].location));
+            $(".education-entry:last").append(HTMLschoolMajor.replace("%data%", data.education.schools[s].majors[0]));
+        }
+        $("#education").append(HTMLonlineClasses);
+        data.education.onlineCourses.reverse();
+        for (var s in data.education.onlineCourses) {
+            var courseString = HTMLonlineStart;
+            courseString += HTMLonlineTitle.replace("%data%", data.education.onlineCourses[s].title);
+            courseString += HTMLonlineSchool.replace("%data%", data.education.onlineCourses[s].school);
+            courseString += HTMLonlineURL.replace("%data%", data.education.onlineCourses[s].url).replace("%data%", data.education.onlineCourses[s].url);
+            $("#education").after(courseString);
+        }
+    };
 };
 
 new ResumeBuilder().addAll();
