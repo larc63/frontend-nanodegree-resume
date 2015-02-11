2f5977e3136c8f72150e9ee64d3c67f20947b766
Luis Antonio <larc63@gmail.com>
2015-02-11 13:47:41 -0600

    added the rest of the json data to html, bound recalculatables

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,59 +1,37 @@
-/*global $*/
-/*global bio*/
-/*global work*/
-/*global projects*/
-/*global education*/
-/*global googleMap*/
-//
-//function ResumeBuilder() {
-//    "use strict";
-//    var i, job;
-//    this.addAll = function () {
-//        bio.displayBio();
-//        bio.displayContacts();
-//        bio.displaySkills();
-//        projects.displayProjects();
-//        work.displayWorkExperience();
-//        education.displayEducation();
-//        $("#mapDiv").append(googleMap);
-//        this.hideEmptyDivs();
-//    };
-//
-//    this.hideEmptyDivs = function () {
-//        if (document.getElementsByClassName('flex-item').length === 0) {
-//            document.getElementById('topContacts').style.display = 'none';
-//        }
-//        if (document.getElementsByTagName('h1').length === 0) {
-//            document.getElementById('header').style.display = 'none';
-//        }
-//        if (document.getElementsByClassName('work-entry').length === 0) {
-//            document.getElementById('workExperience').style.display = 'none';
-//        }
-//        if (document.getElementsByClassName('project-entry').length === 0) {
-//            document.getElementById('projects').style.display = 'none';
-//        }
-//        if (document.getElementsByClassName('education-entry').length === 0) {
-//            document.getElementById('education').style.display = 'none';
-//        }
-//        if (document.getElementsByClassName('flex-item').length === 0) {
-//            document.getElementById('letsConnect').style.display = 'none';
-//        }
-//        if (document.getElementById('google-map') === undefined) {
-//            document.getElementById('mapDiv').style.display = 'none';
-//        }
-//    };
-//}
-//
-//new ResumeBuilder().addAll();
+var Course = function (data) {
+    this.title = data.title;
+    this.school = data.school;
+    this.dates = data.dates;
+    this.url = data.url;
+}
 
-var Job(data){
+var School = function (data) {
+    this.name = data.name;
+    this.location = data.location;
+    this.degree = data.degree;
+    this.major = data.major;
+    this.dates = data.dates;
+    this.url = data.url;
+}
+
+var Project = function (data) {
+    this.name = data.name;
+    this.description = data.description;
+}
+
+var Job = function (data) {
     this.employer = data.employer;
     this.title = data.title;
     this.location = data.location;
     this.dates = data.dates;
     this.brief = data.brief;
     this.description = data.description;
-    this.roles = data.roles;
+    this.roles = ko.observableArray([]);
+    for (r in data.roles) {
+        this.roles.push({
+            role: data.roles[r]
+        });
+    }
     this.url = data.url;
 };
 
@@ -61,14 +39,8 @@ var ViewModel = function () {
     var self = this;
     this.name = ko.observable(data.bio.name);
     this.role = ko.observable(data.bio.role);
-    this.skills = ko.observableArray([]);
-    for (i in data.bio.skills) {
-        this.skills.push({
-            skill: data.bio.skills[i]
-        });
-    }
 
-    this.rows = ko.computed(function () {
+    this.skills = ko.computed(function () {
         var result = [],
             row,
             colLength = 2;
@@ -81,7 +53,9 @@ var ViewModel = function () {
                 }
                 row = [];
             }
-            row.push({skill: data.bio.skills[i]});
+            row.push({
+                skill: data.bio.skills[i]
+            });
         }
 
         //push the final row  
@@ -92,6 +66,60 @@ var ViewModel = function () {
         return result;
     }, this);
 
+    this.jobs = ko.observableArray([]);
+    for (j in data.work.jobs) {
+        this.jobs.push(new Job(data.work.jobs[j]));
+    }
+
+    this.projects = ko.observableArray([]);
+    for (p in data.projects.projects) {
+        this.projects.push(new Project(data.projects.projects[p]));
+    }
+
+    this.schools = ko.observableArray([]);
+    for (p in data.education.schools) {
+        this.schools.push(new School(data.education.schools[p]));
+    }
+
+    this.courses = ko.observableArray([]);
+    for (p in data.education.onlineCourses) {
+        this.courses.push(new Course(data.education.onlineCourses[p]));
+    }
+    this.dummyObservable = ko.observable();
+    this.coursesDisplay = ko.computed(function () {
+        var result = [],
+            row,
+            colLength = window.innerWidth < 1000 ? 1 : 2;
+        this.dummyObservable();
+        //loop through items and push each item to a row array that gets pushed to the final result
+        for (var i = 0, j = data.education.onlineCourses.length; i < j; i++) {
+            if (i % colLength === 0) {
+                if (row) {
+                    result.push(row);
+                }
+                row = [];
+            }
+            row.push({
+                course: data.education.onlineCourses[i]
+            });
+        }
+
+        //push the final row  
+        if (row) {
+            result.push(row);
+        }
+
+        return result;
+    }, this);
 };
 
-ko.applyBindings(new ViewModel()); // This makes Knockout get to work
+ViewModel.prototype.resizeListener = function () {
+    this.dummyObservable.notifySubscribers();
+}
+var vm = new ViewModel();
+
+ko.applyBindings(vm); // This makes Knockout get to work
+
+window.addEventListener('resize', function () {
+    vm.resizeListener();
+}, true);
