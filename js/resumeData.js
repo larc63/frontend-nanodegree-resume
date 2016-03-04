3665dd60077eca051d67950602521195d67a0d7e
Luis Antonio <larc63@gmail.com>
2016-03-03 21:26:08 -0600

    Trimmed some data for submission and added the parsed data from mind

--- js/resumeData.js
+++ js/resumeData.js
@@ -4,6 +4,7 @@ var data = {
         "role": "Senior Software Developer",
         "skills": [
         "Java",
+            "Swift",
         "HTML",
         "CSS",
         "JavaScript",
@@ -11,12 +12,12 @@ var data = {
         "C++",
         "Objective-C",
         "iOS",
-        "Android (NDK, AOSP)",
-        "J2ME",
-        "Java Native Interface",
-        "Lua",
-        "JSObject",
-        "Ant"
+//        "Android (NDK, AOSP)",
+//        "J2ME",
+//        "Java Native Interface",
+//        "Lua",
+//        "JSObject",
+//        "Ant"
     ],
         "contacts": {
             "mobile": "+1 972-850-6310",
@@ -36,12 +37,13 @@ var data = {
                 "brief": "Design and implement functional models of the client's new products and features.",
                 "description": "Create functional models for new features to be implemented on future releases of the company's client product lines. The product lines have source code written in different languages and architectures, as a System Engineer, the design and implementation of functionalities often require visiting different layers of the system, be it communicating from devices to desktop software, making portable documents that work on multiple architectures and form factors that the software runs on to creating stand-alone device emulators",
                 "roles": [
+                    "Did porting of a proprietary communications protocol that uses USB in order to have it work on a Chrome App for Chrome OS",
                 "Worked with the team responsible for re-imagining and rewriting legacy products into more modern UI/UX and their corresponding architectures and languages.",
                 "Part of the team that implemented HTML5 emulators for z80-based devices which have gone through our client's pilots involving 800k users without any reported issues.",
-                "Compiled and modified the AOSP to research VNC server possibilities; test target at the time was an OMAP dev board.",
-                "Worked in conjunction with the product managers and development leads to determine software requirements.",
+//                "Compiled and modified the AOSP to research VNC server possibilities; test target at the time was an OMAP dev board.",
+//                "Worked in conjunction with the product managers and development leads to determine software requirements.",
                 "Worked in conjunction with the development team to refactor the usage of JNI, improving performance and memory usage.",
-                "Designed and implemented some of the infrastructure necessary to create new classroom activities using the Lua scripting language. The C counterpart for the Lua bindings, as well as the Lua engine itself gets cross-compiled to run on different platform including iOS, Nucleus and desktop operating systems."
+//                "Designed and implemented some of the infrastructure necessary to create new classroom activities using the Lua scripting language. The C counterpart for the Lua bindings, as well as the Lua engine itself gets cross-compiled to run on different platform including iOS, Nucleus and desktop operating systems."
             ],
                 "url": "http://education.ti.com"
         },
@@ -117,12 +119,21 @@ var data = {
                 "name": "Udacity",
                 "location": "Online",
                 "degree": "Nanodegree",
+                "major": "iOS developer",
+                "dates": "2015 - Present",
+                "url": "http://www.udacity.com/"
+        },
+            {
+                "name": "Udacity",
+                "location": "Online",
+                "degree": "Nanodegree",
                 "major": "Front-end web developer",
                 "dates": "2014 - 2015",
                 "url": "http://www.udacity.com/"
         },
             {
-                "name": "Centro de Investigación Científica y Educación Superior de Ensenada",
+                //                "name": "Centro de Investigación Científica y Educación Superior de Ensenada",
+                "name": "CICESE",
                 "location": "Ensenada, B. C., Mexico",
                 "degree": "Master's",
                 "major": "Electronics and Telecommunications with specialization in control systems and automation",
@@ -130,7 +141,8 @@ var data = {
                 "url": "http://www.cicese.edu.mx/"
         },
             {
-                "name": "Instituto Tecnológico y de Estudios Superiores de Monterrey ",
+                //                "name": "Instituto Tecnológico y de Estudios Superiores de Monterrey ",
+                "name": "ITESM",
                 "location": "Monterrey, N. L.",
                 "degree": "Bachelor's",
                 "major": "Electronics Systems Engineering",
@@ -140,61 +152,96 @@ var data = {
     ],
         "onlineCourses": [
             {
-                "title": "JavaScript Design Patterns",
+                "title": "iOS Persistence and Core Data",
                 "school": "Udacity",
                 "dates": "2015",
-                "url": "https://www.udacity.com/course/ud989",
+                "url": "https://www.udacity.com/course/viewer#!/c-ud325-nd",
                 "certificate": "#"
         },
             {
-                "title": "Website Performance Optimization",
+                "title": "iOS Networking with Swift",
                 "school": "Udacity",
-                "dates": "2014",
-                "url": "https://www.udacity.com/course/ud884",
-                "certificate": "res//UD884.pdf"
-        },
-            {
-                "title": "Object-Oriented JavaScript",
-                "school": "Udacity",
-                "dates": "2014",
-                "url": "https://www.udacity.com/course/ud015",
-                "certificate": "res//UD015.pdf"
-        },
-            {
-                "title": "Javascript Basics",
-                "school": "Udacity",
-                "dates": "2014",
-                "url": "https://www.udacity.com/course/ud804",
-                "certificate": "res//UD804.pdf"
-        },
-            {
-                "title": "Intro to HTML and CSS",
-                "school": "Udacity",
-                "dates": "2014",
-                "url": "https://www.udacity.com/course/ud304",
-                "certificate": "res/UD304.pdf"
-        },
-            {
-                "title": "HTML5 Game Development",
-                "school": "Udacity",
-                "dates": "2014",
-                "url": "https://www.udacity.com/course/cs255",
-                "certificate": "res/CS255.pdf"
-        },
-            {
-                "title": "jQuery",
-                "school": "Codecademy",
-                "dates": "2013",
-                "url": "http://www.codecademy.com/tracks/jquery",
+                "dates": "2015",
+                "url": "https://www.udacity.com/course/viewer#!/c-ud421-nd",
                 "certificate": "#"
         },
             {
-                "title": "JavaScript",
-                "school": "Codecademy",
-                "dates": "2013",
-                "url": "http://www.codecademy.com/tracks/javascript",
+                "title": "UIKit Fundamentals I",
+                "school": "Udacity",
+                "dates": "2015",
+                "url": "https://www.udacity.com/course/viewer#!/c-ud788b-nd",
                 "certificate": "#"
         },
+            {
+                "title": "UIKit Fundamentals II",
+                "school": "Udacity",
+                "dates": "2015",
+                "url": "https://www.udacity.com/course/viewer#!/c-ud788a-nd",
+                "certificate": "#"
+        },
+            {
+                "title": "Intro to iOS App Development Swift",
+                "school": "Udacity",
+                "dates": "2015",
+                "url": "https://www.udacity.com/course/ud585",
+                "certificate": "#"
+        },
+//{
+//    "title": "JavaScript Design Patterns",
+//    "school": "Udacity",
+//    "dates": "2015",
+//    "url": "https://www.udacity.com/course/ud989",
+//    "certificate": "#"
+//},
+//{
+//    "title": "Website Performance Optimization",
+//    "school": "Udacity",
+//    "dates": "2014",
+//    "url": "https://www.udacity.com/course/ud884",
+//    "certificate": "res//UD884.pdf"
+//},
+//{
+//    "title": "Object-Oriented JavaScript",
+//    "school": "Udacity",
+//    "dates": "2014",
+//    "url": "https://www.udacity.com/course/ud015",
+//    "certificate": "res//UD015.pdf"
+//},
+//{
+//    "title": "Javascript Basics",
+//    "school": "Udacity",
+//    "dates": "2014",
+//    "url": "https://www.udacity.com/course/ud804",
+//    "certificate": "res//UD804.pdf"
+//},
+//{
+//    "title": "Intro to HTML and CSS",
+//    "school": "Udacity",
+//    "dates": "2014",
+//    "url": "https://www.udacity.com/course/ud304",
+//    "certificate": "res/UD304.pdf"
+//},
+//{
+//    "title": "HTML5 Game Development",
+//    "school": "Udacity",
+//    "dates": "2014",
+//    "url": "https://www.udacity.com/course/cs255",
+//    "certificate": "res/CS255.pdf"
+//},
+//{
+//    "title": "jQuery",
+//    "school": "Codecademy",
+//    "dates": "2013",
+//    "url": "http://www.codecademy.com/tracks/jquery",
+//    "certificate": "#"
+//},
+//{
+//    "title": "JavaScript",
+//    "school": "Codecademy",
+//    "dates": "2013",
+//    "url": "http://www.codecademy.com/tracks/javascript",
+//    "certificate": "#"
+//},
             {
                 "title": "Software Testing",
                 "school": "Udacity",
@@ -225,4 +272,85 @@ var data = {
         }
     ]
     }
-}
+};
+
+
+var data2 = {
+    "education": {
+        "schools": ["Udacity iOS developer nanodegree", "Udacity Front-end developer nanodegree", "CICESE M. Sc. Electronics and Telecommunications", "ITESM Bs. in Electronic Systems Engineering", "Courses"]
+    },
+    "work_experience": {
+        "jobs": ["Dextra Technologies 2010-present", "Digital Chocolate 2009-10", "Lemonquest 2008-09", "Gameloft 2005-08", "Webcel 2002-2004", "Soyforaneo.com"]
+    },
+    "projects": {
+        "projects": [{
+            "details": ["Designed and built a virtual keypad for an emulated device over the internet using html5 and java."],
+            "title": "Remote Keyboard (Wireless) for Texas Instruments via Dextra Technologies - 2015-16"
+        }, {
+            "details": ["Ported the communications layer on a utility that talks to TI calculators using Chrome's USB API."],
+            "title": "Chrome App for Texas Instruments via Dextra Technologies - 2015-16"
+        }, {
+            "details": ["Designed and implemented the adapter to enable use of a usb connected keypad with emulated hardware in a java/javascript desktop application."],
+            "title": "Remote Keyboard for Texas Instruments via Dextra Technologies - 2014"
+        }, {
+            "details": ["Determined the risk associated with updating the tools used to build the desktop products.", "https://education.ti.com/en/us/software/search/ti-84-plus-ce"],
+            "title": "Toolchain update investigation for Texas Instruments via Dextra Technologies - 2014"
+        }, {
+            "details": ["Worked closely with the Product Managers to write and review user requirements", "Designed and implemented features related to interacting with emulated hardware", "https://education.ti.com/en/us/products/computer_software/ti-smartview-software/ti-smartview-ce-emulator-software-for-the-ti-84-plus-family/tabs/overview"],
+            "title": "TI Smartview for Texas Instruments via Dextra Technologies - 2013-14"
+        }, {
+            "details": ["Worked closely with the Product Managers to write and review User Requirements", "Designed and implemented features related to usb connectivity for the desktop utility used on Windows and Mac OS", "https://education.ti.com/en/us/products/computer_software/connectivity-software/ti-connect-ce-software/tabs/overview"],
+            "title": "TI Connect for Texas Instruments via Dextra Technologies - 2013"
+        }, {
+            "details": ["Worked closely with the Product Managers to write and review User Requirements", "Ported z80-based calculator emulators from Java and C to Javascript", "Pilots involving 800k users without any reported issues.", "https://education.ti.com/en/us/about/press-center/press-listing-content-module/6-20-2013-ccss"],
+            "title": "ExamCalcs for Texas Instruments via Dextra Technologies - 2013-15"
+        }, {
+            "details": ["Reviewed requirements for homologating the way documents are displayed accross different platforms/screen sizes.", "https://education.ti.com/en/us/products/apps/ti-nspire-app-for-ipad/tabs/overview"],
+            "title": "Document standarization on iOS for Texas Instruments via Dextra Technologies - 2012"
+        }, {
+            "details": ["Researched vnc server options for an OMAP-based android device"],
+            "title": "AOSP VNC server for Texas Instruments via Dextra Technologies - 2012"
+        }, {
+            "details": ["Designed and implemented the infrastructure for creating new classroom activities using the Lua scripting language.", "The C counterpart for the Lua bindings, as well as the Lua engine itself gets cross-compiled to run on different platform including iOS, Nucleus and desktop operating systems.", "https://education.ti.com/sites/US/downloads/pdf/TI-Nspire%20Navigator%203%202%20Release%20Notes.pdf"],
+            "title": "Image Question for Texas Instruments via Dextra Technologies - 2011-12"
+        }, {
+            "title": "Multithreading refactor for Texas Instruments via Dextra Technologies - 2011-12"
+        }, {
+            "details": ["Ported iPhone implementation of OpenGL game to Brew", "https://itunes.apple.com/us/app/3d-rollercoaster-rush/id319575672"],
+            "title": "Rollercoaster game for Digital Chocolate 2010"
+        }, {
+            "details": ["Made the necessary changes for the recently created game to work on low-end J2ME devices", "https://play.google.com/store/apps/details?id=com.digitalchocolate.androidchivoflexion"],
+            "title": "Space invaders for Digital Chocolate 2010"
+        }, {
+            "details": ["Designed and developed a reusable library that provides an API for in-app-purchases using Brew.", "Sprint U. S. Campaign"],
+            "title": "BREW in app purchase library for Digital Chocolate 2010"
+        }, {
+            "details": ["Wrote pedometer and fitness tips application for the iPhone"],
+            "title": "iFit for Lemonquest 2009"
+        }, {
+            "details": ["Ported the directx graphics engine impementation to the iPhone's OpenGL ES framework", "Implemente 3d audio library that maps the user's location to reproduce sounds in a 3d-mapped manner.", "Translated levels originally designed for desktop/mouse to be usable on a touch device", "Game was awarded a bronze medal by pocketgamer.uk http://www.pocketgamer.co.uk/r/iPhone/Circulate+%28iPhone%29/review.asp?c=11151&tab=tips"],
+            "title": "Circulate for Lemonquest 2009"
+        }, {
+            "details": ["Ported the directx impementation to the iPhone's OpenGL ES framework"],
+            "title": "Circulate Prologue for Lemonquest 2008"
+        }, {
+            "details": ["Developed and ported first prototype for the brickbreaker game using the celestial framework for iPhone", "After getting the first company-wide prototype to work, our office was able to take on more projects than what would have originally been assigned to us."],
+            "title": "Blockbreaker for iPhone for Gameloft - 2007-08"
+        }, {
+            "details": ["Implemented minigames within the game engine", "Made the necessary changes for the recently created game to work on low-end J2ME devices", "https://www.youtube.com/watch?v=_Kf7oVgPweg"],
+            "title": "Bikini Volleyball for Gameloft - 2007"
+        }, {
+            "details": ["Ported games from different sources for a few lower-end java devices"],
+            "title": "3d porting on Sprint devices for Gameloft - 2006-07"
+        }, {
+            "details": ["Ported games from different sources for a few lower-end java devices that had a 100k jar size limitation"],
+            "title": "Virgin Mobile Projects for Gameloft 2005-2006"
+        }, {
+            "details": ["Wrote a relegious-focused application that shows images, plays sounds and uses the accelerometer to interact with the user."],
+            "title": "iAarti for Katha Films 2008-09"
+        }, {
+            "details": ["https://itunes.apple.com/us/app/50-chinese-words-learn-to/id312982180?mt=8", "Wrote an application that reproduces the strokes necessary for writing chinese characters as well as playing their sound", "Developed a Chinese character writing tutor for the iPhone"],
+            "title": "50 Chinese Words for One Element - 2009"
+        }]
+    }
+};
